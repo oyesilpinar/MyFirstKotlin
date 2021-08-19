76ae480d6641228bc578396fa92ac0fcf10d8292
Oguzhan Yeşilpınar <41451987+oyesilpinar@users.noreply.github.com>
2021-08-19 17:25:48 +0300

    Initial commit

---
+++ app/src/main/java/com/ougen/myfirstkotlin/JsonData.java
@@ -0,0 +1,16 @@
+package com.ougen.myfirstkotlin;
+
+// import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
+// import com.fasterxml.jackson.annotation.JsonProperty; // version 2.11.1
+/* ObjectMapper om = new ObjectMapper();
+Root root = om.readValue(myJsonString), Root.class); */
+public class JsonData{
+    public String q;
+    public String opt1;
+    public String opt2;
+    public String opt3;
+    public String answer;
+}
+
+
+
---
+++ app/src/main/java/com/ougen/myfirstkotlin/Root.java
@@ -0,0 +1,5 @@
+package com.ougen.myfirstkotlin;
+
+public class Root{
+    public List<JsonData> jsonData;
+}
